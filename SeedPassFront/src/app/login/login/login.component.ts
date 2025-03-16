d52ff3b401bf70bf00eecb04187d31d21d187b9e
ndongombathie <ndongo.mbath@univ-thies.sn>
2025-03-16 22:44:11 +0000

    ajout de l'interface inscription

--- SeedPassFront/src/app/login/login/login.component.ts
+++ SeedPassFront/src/app/login/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators,ReactiveFormsModule} from '@angular/forms';
 import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
 import { InscriptionComponent } from "../../inscription/inscription/inscription.component";
+import { FeaturesComponent } from "../../features/features/features.component";
 
 
 @Component({
   selector: 'app-login',
-  imports: [NgClass, ReactiveFormsModule, InscriptionComponent],
+  imports: [NgClass, ReactiveFormsModule, InscriptionComponent, FeaturesComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
