4a61e7c12e4cf23ca0e442e341eb7d9c7e1b48b4
ndongombathie <ndongo.mbath@univ-thies.sn>
2025-04-04 12:07:58 +0000

    mja+

--- SeedPass/routes/api.php
+++ SeedPass/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FamerController;
@@ -14,6 +15,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('seedpass/login',[AuthController::class,'login']);
 
 Route::prefix('ressource')->group(function () {
     Route::apiResource('producteurs', ProductorController::class);
